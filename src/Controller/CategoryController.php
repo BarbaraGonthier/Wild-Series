dd0f7a9423ec176711120e495bd6f8062a8ef564
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-12-02 07:48:03 +0100

    challenge 11

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,6 +16,26 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CategoryController extends AbstractController
 {
+    /**
+     * The controller for the category add form
+     * Correspond à la route /categories/ et au name "category_new"
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
     /**
      * Correspond à la route /categories/ et au name "category_index"
      * @Route("/", name="index")
