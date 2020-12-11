deb6f1a55c93b18858c45cc30ffe140a0e9a5e43
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-12-11 08:13:20 +0100

    challenge service

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Episode;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -10,12 +11,20 @@ use Faker;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
+    private $slugify;
+
+    public function __construct(Slugify $slugify)
+    {
+        $this->slugify = $slugify;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
         for ($i = 0; $i < 60; $i++) {
             $episode = new Episode();
             $episode->setTitle($faker->text(20));
+            $episode->setSlug($this->slugify->generate($episode->getTitle()));
             $episode->setNumber(rand(0,3));
             $episode->setSynopsis($faker->text(200));
             $episode->setSeason($this->getReference('season_'.rand(0,3)));
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Program;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -36,12 +37,20 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
 
         ],
     ];
+    private $slugify;
+
+    public function __construct(Slugify $slugify)
+    {
+        $this->slugify = $slugify;
+    }
+
     public function load(ObjectManager $manager)
     {
         $i=0;
         foreach (self::PROGRAMS as $title => $data) {
             $program = new Program();
             $program->setTitle($title);
+            $program->setSlug($this->slugify->generate($title));
             $program->setSummary($data['summary']);
             $program->setCategory($this->getReference('category_'.rand(0,5)));
             $manager->persist($program);
