7f3525d101d7ca2fd518fc5ac0fc470a18414d8a
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-12-14 08:17:34 +0100

    challenge 18

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -12,6 +12,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -24,7 +26,7 @@ class ProgramController extends AbstractController
      * Correspond à la route /programs/ et au name "program_add"
      * @Route("/new", name="new")
      */
-    public function new(Request $request, Slugify $slugify) : Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer) : Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -35,6 +37,19 @@ class ProgramController extends AbstractController
             $program->setSlug($slug);
             $entityManager->persist($program);
             $entityManager->flush();
+
+            $email = (new Email())
+
+                ->from($this->getParameter('mailer_from'))
+
+                ->to($this->getParameter('mailer_to'))
+
+                ->subject('Une nouvelle série vient d\'être publiée !')
+
+                ->html($this->renderView('program/newProgramEmail.html.twig', ['program' => $program]));
+
+
+            $mailer->send($email);
             return $this->redirectToRoute('program_index');
         }
         return $this->render('program/new.html.twig', [
