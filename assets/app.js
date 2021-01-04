1f117a4a0f45fc2ef607d3744b13a122cf8f0383
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-01-04 07:46:02 +0100

    challenge 26

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,5 @@ import './styles/app.scss';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+const $ = require('jquery');
+require('bootstrap');
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,11 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('index.html.twig');
+    }
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+        ]);
     }
 }
